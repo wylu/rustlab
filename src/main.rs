1b3795d7661aa3b8da134bf30de285c1400e1b82
wylu <158677478@qq.com>
2024-01-02 23:36:08 +0800

    2.2.1 位运算

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,23 @@
 fn main() {
-    // 加法
-    let sum = 5 + 10;
-    println!("sum: {}", sum);
-    // 减法
-    let difference = 95.5 - 4.3;
-    println!("difference: {}", difference);
-    // 乘法
-    let product = 4 * 30;
-    println!("product: {}", product);
-    // 除法
-    let quotient = 56.7 / 32.2;
-    println!("quotient: {}", quotient);
-    // 求余
-    let remainder = 43 % 5;
-    println!("remainder: {}", remainder);
+    // 二进制为00000010
+    let a: i32 = 2;
+    // 二进制为00000011
+    let b: i32 = 3;
 
-    // 编译器会进行自动推导，给予twenty i32的类型
-    let twenty = 20;
-    // 类型标注
-    let twenty_one: i32 = 21;
-    // 通过类型后缀的方式进行类型标注：22是i32类型
-    let twenty_two = 22i32;
+    println!("(a & b) value is {}", a & b);
 
-    // 只有同样类型，才能运算
-    let addition = twenty + twenty_one + twenty_two;
-    println!(
-        "{} + {} + {} = {}",
-        twenty, twenty_one, twenty_two, addition
-    );
+    println!("(a | b) value is {}", a | b);
 
-    // 对于较长的数字，可以用_进行分割，提升可读性
-    let one_million: i64 = 1_000_000;
-    println!("{}", one_million.pow(2));
+    println!("(a ^ b) value is {}", a ^ b);
 
-    // 定义一个f32数组，其中42.0会自动被推导为f32类型
-    let forty_twos = [42.0, 42f32, 42.0_f32];
+    println!("(!b) value is {} ", !b);
 
-    // 打印数组中第一个值，并控制小数位为2位
-    println!("{:.2}", forty_twos[0]);
+    println!("(a << b) value is {}", a << b);
+
+    println!("(a >> b) value is {}", a >> b);
+
+    let mut a = a;
+    // 注意这些计算符除了!之外都可以加上=进行赋值 (因为!=要用来判断不等于)
+    a <<= b;
+    println!("(a << b) value is {}", a);
 }

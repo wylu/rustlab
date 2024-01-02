fdfbf2448f6db405560dba1c4666534b6d1ef829
wylu <158677478@qq.com>
2024-01-02 23:21:37 +0800

    2.2.1 整型溢出

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 fn main() {
-    let x = 5;
-    // 在main函数的作用域内对之前的x进行遮蔽
-    let x = x + 1;
-
-    {
-        // 在当前的花括号作用域内，对之前的x进行遮蔽
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
-
-    println!("The value of x is: {}", x);
+    let a: u8 = 255;
+    let b = a.wrapping_add(20);
+    println!("{}", b); // 19
 }

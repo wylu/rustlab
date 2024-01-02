18946a1941d2f63923528e42d79a8bd62b4f774f
wylu <158677478@qq.com>
2024-01-02 23:38:06 +0800

    2.2.1 序列(Range)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,11 @@
 fn main() {
-    // 二进制为00000010
-    let a: i32 = 2;
-    // 二进制为00000011
-    let b: i32 = 3;
+    for i in 1..=4 {
+        print!("{} ", i);
+    }
+    println!();
 
-    println!("(a & b) value is {}", a & b);
-
-    println!("(a | b) value is {}", a | b);
-
-    println!("(a ^ b) value is {}", a ^ b);
-
-    println!("(!b) value is {} ", !b);
-
-    println!("(a << b) value is {}", a << b);
-
-    println!("(a >> b) value is {}", a >> b);
-
-    let mut a = a;
-    // 注意这些计算符除了!之外都可以加上=进行赋值 (因为!=要用来判断不等于)
-    a <<= b;
-    println!("(a << b) value is {}", a);
+    for i in 'a'..='z' {
+        print!("{} ", i);
+    }
+    println!();
 }

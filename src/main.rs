1deec63a2ecb6b1d73a1332c5aacc3a10a14a5b8
wylu <158677478@qq.com>
2024-01-03 23:35:10 +0800

    2.2.4 函数返回

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-fn main() {
-    another_function(5, 6.1);
+fn plus_five(x: i32) -> i32 {
+    x + 5
 }
 
-fn another_function(x: i32, y: f32) {
+fn plus_or_minus(x: i32) -> i32 {
+    if x > 5 {
+        return x - 5;
+    }
+    x + 5
+}
+
+fn main() {
+    let x = plus_five(5);
     println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
+
+    let x = plus_or_minus(5);
+    println!("The value of x is: {}", x);
 }

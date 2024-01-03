84634a561f2e6e5468322c3f301c779ccc0dcc97
wylu <158677478@qq.com>
2024-01-03 23:31:00 +0800

    2.2.4 函数参数

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,8 @@
 fn main() {
-    let y = {
-        let x = 3;
-        x + 1
-    };
-
-    println!("The value of y is: {}", y);
-
-    assert_eq!(ret_unit_type(), ())
+    another_function(5, 6.1);
 }
 
-fn ret_unit_type() {
-    let x = 1;
-    // if 语句块也是一个表达式，因此可以用于赋值，也可以直接返回
-    // 类似三元运算符，在Rust里我们可以这样写
-    let y = if x % 2 == 1 { "odd" } else { "even" };
-    // 或者写成一行
-    let z = if x % 2 == 1 { "odd" } else { "even" };
-    println!("({}, {}, {})", x, y, z);
+fn another_function(x: i32, y: f32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }

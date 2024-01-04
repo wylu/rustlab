ae9a9728b4d4e5e17ad7adb0293e960094a3e0b1
wylu <158677478@qq.com>
2024-01-04 23:03:15 +0800

    2.3.1 简单介绍 String 类型

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
-use std::thread;
-use std::time;
-
-fn dead_end() -> ! {
-    panic!("你已经到了穷途末路，崩溃吧！");
-}
-
-fn forever() -> ! {
-    loop {
-        let delay = time::Duration::from_secs(1);
-        let now = time::Instant::now();
-        println!("{:?}", now);
-        thread::sleep(delay);
-    }
-}
-
 fn main() {
-    _ = dead_end;
-    forever();
+    let mut s = String::from("hello");
+    s.push_str(", world!"); // push_str() 在字符串后追加字面值
+    println!("{}", s); // 将打印 `hello, world!`
 }
